39c019a74800cff30e4b460e78329027f3a69e87
Peter Fraenkel <pnf@podsnap.com>
2013-10-15 13:01:58 -0400

    Add another fizzbuzz variation

--- src/clj/playground/fizzbuzz.clj
+++ src/clj/playground/fizzbuzz.clj
@@ -13,13 +13,21 @@
 
 
 (defn fb2
-  ([] (rest (fb nil 1 2 4)))
-  ([p n c3 c5]
-     (cons p (lazy-seq (match [c3 c5]
-                              [0  0] (fb "fizzbuzz" (inc n) 2        4)
-                              [0  _] (fb "fizz"     (inc n) 2        (dec c5))
-                              [_  0] (fb "buzz"     (inc n) (dec c3) 4)
-                              :else  (fb n          (inc n) (dec c3) (dec c5)))))))
+  ([] (rest (fb2 1 nil 2 4)))
+  ([n p c3 c5]
+     (cons p (lazy-seq (apply fb2 (inc n)
+                              (match [c3 c5]
+                                     [0  0] [ "fizzbuzz"  2        4]
+                                     [0  _] [ "fizz"      2        (dec c5)]
+                                     [_  0] [ "buzz"      (dec c3) 4]
+                                     :else  [ n           (dec c3) (dec c5)]))))))
 
 
 
+(defn fb3
+  ([] (rest (fb3 1 0 [nil 2] [nil 4])))
+  ([n np [p3 c3] [ p5 c5]]
+     (cons (str p3 p5 (when-not (or p3 p5) np))
+           (lazy-seq (fb3 (inc n) n
+                          (if (zero? c3) ["fizz" 2] [nil (dec c3)])
+                          (if (zero? c5) ["buzz" 4] [nil (dec c5)]))))))
