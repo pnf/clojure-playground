87a0196b0cf32faef849a4f9591e639baab467ec
Peter Fraenkel <pnf@podsnap.com>
2013-12-08 21:24:15 -0500

    Add more map examples

--- src/clj/playground/seqs.clj
+++ src/clj/playground/seqs.clj
@@ -40,8 +40,13 @@
   (map f1 (iterate f2 ss)))
 
 (defn mymap  [f c] (reduce #(concat %1 (list (f %2))) () c))
-(defn mymap3[f coll]
+(defn mymap3 [f coll]
   (cons (f (first coll)) (lazy-seq (mymap3 f (rest coll)))))
+
+(defn mymap2 [f coll]
+  (if (seq coll)
+    (cons (f (first coll)) (lazy-seq (mymap2 f (rest coll))))
+    ()))
 
 (defn inv6 [ss]
   (map (partial mymap3 first)
@@ -59,8 +64,16 @@
 ; out of memory
 #_(first (nth (iterate (partial mymap inc) (range) ) 100000))
 
+; (1000 1001 1002 1003 1004)
+#_(nth (iterate (partial mymap2 inc) (range 5)) 1000)
+; stack overflow
+#_(nth (iterate (partial mymap2 inc) (range 5)) 10000)
+
 ; 100000
 #_(first (nth (iterate (partial mymap3 inc) (range) ) 100000))
+; stackoverlflow
+#_(second (nth (iterate (partial mymap3 inc) (range) ) 100000))
+
 
 
 
