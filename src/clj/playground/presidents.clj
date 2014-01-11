f0c33949eeafe990b07e5f30828dadbe94cc9f74
Peter Fraenkel <pnf@podsnap.com>
2014-01-11 17:34:45 -0500

    Was matching (winners-losers)&!losers rather than winners&!(losers-winners)

--- src/clj/playground/presidents.clj
+++ src/clj/playground/presidents.clj
@@ -3,9 +3,11 @@
             [clojure.set :as sets])
   (:import (java.lang Math)))
 
-(def losers (set (str/split "clinton jefferson adams pinckney pinckney clinton king adams jackson adams clay vanburen vanburen clay cass scott fremont breckinridge mcclellan seymour greeley tilden hancock blaine cleveland harrison bryan bryan parker bryan roosevelt hughes cox davis smith hoover landon wilkie dewey dewey stevenson stevenson nixon goldwater humphrey mcgovern ford carter mondale dukakis bush dole gore kerry mccain romney" #" ")))
+(def winners (set (str/split "washington adams jefferson jefferson madison madison monroe monroe adams jackson jackson vanburen harrison polk taylor pierce buchanan lincoln lincoln grant grant hayes garfield cleveland harrison cleveland mckinley mckinley roosevelt taft wilson wilson harding coolidge hoover roosevelt roosevelt roosevelt roosevelt truman eisenhower eisenhower kennedy johnson nixon nixon carter reagan reagan bush clinton clinton bush bush obama obama" #" ")))
 
-(def winners (sets/difference (set (str/split "washington adams jefferson jefferson madison madison monroe monroe adams jackson jackson vanburen harrison polk taylor pierce buchanan lincoln lincoln grant grant hayes garfield cleveland harrison cleveland mckinley mckinley roosevelt taft wilson wilson harding coolidge hoover roosevelt roosevelt roosevelt roosevelt truman eisenhower eisenhower kennedy johnson nixon nixon carter reagan reagan bush clinton clinton bush bush obama obama" #" ")) losers))
+(def losers (sets/difference  (set (str/split "clinton jefferson adams pinckney pinckney clinton king adams jackson adams clay vanburen vanburen clay cass scott fremont breckinridge mcclellan seymour greeley tilden hancock blaine cleveland harrison bryan bryan parker bryan roosevelt hughes cox davis smith hoover landon wilkie dewey dewey stevenson stevenson nixon goldwater humphrey mcgovern ford carter mondale dukakis bush dole gore kerry mccain romney" #" ")) winners ))
+
+
 
 
 (def initial (set (map #(str "^" % "$") winners)))
@@ -51,11 +53,11 @@
 (def perturbations
   (reduce #(conj %1 [(+ (first (last  %1)) (first %2)) (second %2)])
           [[0.0 identity]]
-          [[1 add-back]
-           [33 yank]
-           [33 dot]
-           [33 decapitate]
-           [100 chop]]))
+          [[100 add-back]
+           [500 yank]
+           [100 dot]
+           [100 decapitate]
+           [500 chop]]))
 
 ; Apply a random perturbation
 (defn perturb [res]
@@ -71,14 +73,16 @@
 ;  E    - its energy
 ;  T    - current temperature
 ;  dT   - will multiply T by (1-dT) at each step
-(defn step [[res E T dT]]
+(defn step [[res E T dT n dn]]
   (let [res2     (perturb res)
         E2       (energy res2)
-        [res E]  (if (and (check res2) (> (Math/exp (/  (- E E2) T)) (rand))) [res2 E2] [res E])  ]
-    [res E (* T (- 1.0 dT)) dT]))
-
+        [res E]  (if (and (check res2) (> (Math/exp (/  (- E E2) T)) (rand))) [res2 E2] [res E])
+        ret      [res E (* T (- 1.0 dT)) dT (inc n) dn]
+        rx       (str/join "|" res)]
+    (when (zero? (mod n dn)) (println (count rx) rx T))
+    ret
+    ))
 ; (nth (iterate step [initial (energy initial) 100.0 0.00004]) 200000)
-;           n..y|ta|p.l|ra|ard|ma|w....n|ga|...r.e|h....$|..ol
-;Norvig's : a.a|a..i|j|li|a.t|i..n|bu|oo|n.e|ay.|r.e$|tr|ls|po|v.l
-
-
+; (nth (iterate step [initial (energy initial) 5.0 0.0000002 0 100000]) 20000000)
+;PF : n.e|ho|ls|a.t|a..i|j|^n|bu|v.l|ma|a.a|ay.|r.e$|li|po
+;PN : a.a|a..i|j|li|a.t|i..n|bu|oo|n.e|ay.|r.e$|tr|ls|po|v.l
