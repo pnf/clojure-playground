0812fbd6d94d549a3ebeda118af1b55d06489f62
Peter Fraenkel <pnf@podsnap.com>
2014-01-11 13:18:14 -0500

    More comments

--- src/clj/playground/presidents.clj
+++ src/clj/playground/presidents.clj
@@ -37,14 +37,17 @@
 ; Randomly remove an entire regexp
 (defn yank [res] (disj res (rand-nth (seq res))))
 
+; Randomly add back a winner
 (defn add-back [res]
   (conj res (str "^" (rand-nth (seq  winners)) "$")))
 
+; Randomly remove first or last (equivalent to some combination of chop and yank)
 (defn decapitate [res]
   (let [cand (get-rand-gt1 res)]
     (when cand
       (conj (disj res cand) (str/join ((nth [next drop-last] (rand-int 2)) cand))))))
 
+; Define perturbations by relative probability
 (def perturbations
   (reduce #(conj %1 [(+ (first (last  %1)) (first %2)) (second %2)])
           [[0.0 identity]]
@@ -54,20 +57,27 @@
            [33 decapitate]
            [100 chop]]))
 
+; Apply a random perturbation
 (defn perturb [res]
   (let [r (rand (first (last perturbations)))
         f (second (first (filter #(< r (first %)) perturbations)))]
     (f res)))
 
+; Ooh, let's be all physicsy.  Just adding up length of the regexp.
 (defn energy [res] (+ (count res) (reduce + (map count res))))
 
-
+; An iterative step of MH
+;  res  - set of |-able regexp components
+;  E    - its energy
+;  T    - current temperature
+;  dT   - will multiply T by (1-dT) at each step
 (defn step [[res E T dT]]
   (let [res2     (perturb res)
         E2       (energy res2)
         [res E]  (if (and (check res2) (> (Math/exp (/  (- E E2) T)) (rand))) [res2 E2] [res E])  ]
     [res E (* T (- 1.0 dT)) dT]))
 
-; "n..y|ta|p.l|ra|ard|ma|w....n|ga|...r.e|h....$|..ol"
 ; (nth (iterate step [initial (energy initial) 100.0 0.00004]) 200000)
+; "n..y|ta|p.l|ra|ard|ma|w....n|ga|...r.e|h....$|..ol"
+
 
